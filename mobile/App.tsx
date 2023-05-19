a48edf16039bfbc207e5e99f95f134ea9aeac8fb
ThiagoLG <thiago.lg53@gmail.com>
2023-05-19 01:05:28 -0300

    [mobile] - Created principal page + installed fonts and dependencies to svg + realized some configs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,68 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBr from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import React from 'react'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) return null
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-5xl font-bold text-zinc-50">Hello World!</Text>
+    <ImageBackground
+      source={blurBr}
+      className="relative flex-1 items-center bg-gray-900 p-8"
+      imageStyle={{
+        position: 'absolute',
+        left: '-120%',
+      }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Your time capsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Collect memorable moments from your journey and share them with the
+            world{' '}
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Register Memory
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-300">
+        Maked with 💜 on Rocketseat{"'"}s NLW
+      </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
