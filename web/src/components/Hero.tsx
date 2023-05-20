26b02deab0ebdbc4e7671870d215c128e396ef36
ThiagoLG <thiago.lg53@gmail.com>
2023-05-20 10:36:18 -0300

    [web] - Componentizing the home layout

---
+++ web/src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import * as nlwSpacetimeLogoSvg from "../assets/nlw-spacetime-logo.svg";
+
+export function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwSpacetimeLogoSvg} alt={"Application Logo"} />
+
+      <div className="max-w-[420px] space-y-1">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Your time capsule
+        </h1>
+        <p className="text-lg leading-relaxed">
+          Collect memorable moments of your journey and share share them with
+          the world!
+        </p>
+      </div>
+      <a
+        href=""
+        className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+      >
+        REGISTER MOMORY
+      </a>
+    </div>
+  );
+}
