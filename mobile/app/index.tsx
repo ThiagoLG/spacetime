ba23034f99ece8f2c31ab2b86fe2cf4dc0b8a586
ThiagoLG <thiago.lg53@gmail.com>
2023-05-20 16:26:26 -0300

    [mobile] - Implemented auth flow

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -6,21 +6,81 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBr from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import React from 'react'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+
+import { api } from '../src/lib/api'
+import blurBr from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import {
+  DiscoveryDocument,
+  makeRedirectUri,
+  useAuthRequest,
+} from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 
 const StyledStripes = styled(Stripes)
 
+const discovery: DiscoveryDocument = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/47f17d66e969949b2a7e',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '47f17d66e969949b2a7e',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetimenlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+
+    // .then((response) => {
+    //   const { token } = response.data
+    //   SecureStore.setItemAsync('token', token)
+    // })
+    // .catch((error) => console.error(error))
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetimenlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) return null
 
@@ -51,6 +111,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => promptAsync()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Register Memory
