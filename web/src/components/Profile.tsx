5dec41139950bd59263c00b8e1be08cb915aa351
ThiagoLG <thiago.lg53@gmail.com>
2023-05-20 19:28:11 -0300

    [web] - Created default layout + logout action + add new memory page layout

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -15,7 +15,10 @@ export function Profile() {
 
       <p className="max-w-[170px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-500">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-500"
+        >
           Logout
         </a>
       </p>
