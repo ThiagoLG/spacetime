7af3533d7c65b405087731f91e8f403fb237ed4e
ThiagoLG <thiago.lg53@gmail.com>
2023-05-25 09:03:09 -0300

    [server] - Implementing route to upload files

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,23 @@ import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "path";
 
 const app = fastify();
 
 app.register(cors, {
   origin: true, // limit URLs that can access the APIS
 });
-
+app.register(multipart);
 app.register(memoriesRoutes);
 app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(jwt, {
   secret: `${process.env.JWT_SECRET}`,
